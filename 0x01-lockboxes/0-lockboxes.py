e6512d549001205c66a1ef217b6ffa90070a1001
123harri <harrietwanjiku90@gmail.com>
2024-07-03 18:32:16 +0000

    a

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -11,11 +11,11 @@ can be unlocked given a list of boxes with keys.
 def canUnlockAll(boxes):
     """
     Determines if all boxes can be unlocked.
-    
+
     Args:
         boxes (list of lists): A list of boxes,
         each containing a list of keys.
-    
+
     Returns:
         bool: True if all boxes can be unlocked, False otherwise.
     """
@@ -23,12 +23,12 @@ def canUnlockAll(boxes):
     unlocked = [False] * n
     unlocked[0] = True
     keys = [0]
-    
+
     while keys:
         current_key = keys.pop()
         for key in boxes[current_key]:
             if key < n and not unlocked[key]:
                 unlocked[key] = True
                 keys.append(key)
-    
+
     return all(unlocked)
