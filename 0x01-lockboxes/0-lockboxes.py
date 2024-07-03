59674b4adb6c6e44ef91a0aad20a27d059008a83
123harri <harrietwanjiku90@gmail.com>
2024-07-03 18:27:15 +0000

    a

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+0-lockboxes module
+
+This module provides the canUnlockAll function to
+determine if all boxes
+can be unlocked given a list of boxes with keys.
+"""
+
+
+def canUnlockAll(boxes):
+    """
+    Determines if all boxes can be unlocked.
+    
+    Args:
+        boxes (list of lists): A list of boxes,
+        each containing a list of keys.
+    
+    Returns:
+        bool: True if all boxes can be unlocked, False otherwise.
+    """
+    n = len(boxes)
+    unlocked = [False] * n
+    unlocked[0] = True
+    keys = [0]
+    
+    while keys:
+        current_key = keys.pop()
+        for key in boxes[current_key]:
+            if key < n and not unlocked[key]:
+                unlocked[key] = True
+                keys.append(key)
+    
+    return all(unlocked)
---
+++ 0x01-lockboxes/README.md
@@ -0,0 +1,19 @@
+# Lockboxes
+
+This project contains a Python module to determine if a set of boxes can be unlocked given the keys contained in each box.
+
+## Function
+
+### `canUnlockAll`
+
+```python
+def canUnlockAll(boxes):
+    """
+    Determines if all boxes can be unlocked.
+    
+    Args:
+        boxes (list of lists): A list of boxes, each containing a list of keys.
+    
+    Returns:
+        bool: True if all boxes can be unlocked, False otherwise.
+    """
