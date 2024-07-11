2a29efa2da3bc90c832f8d8986d94c7b2ff6ba6c
123harri <harrietwanjiku90@gmail.com>
2024-07-11 10:10:12 +0000

    a

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,25 +1,31 @@
 #!/usr/bin/python3
 """
-Calculates the minimum number of operations
-needed to achieve exactly n 'H' characters
+Calculates the minimum number of operations needed to
+achieve exactly n 'H' characters
 in a text file using Copy All and Paste operations.
 """
 
+import math
+
 
 def minOperations(n):
+    """
+    minOperations
+    Gets fewest # of operations needed to result in exactly n H characters
+    """
     if n <= 1:
         return n
 
-    operations = 0
-    divisor = 2
+    min_ops = float('inf')
 
-    while n > 1:
-        while n % divisor == 0:
-            operations += divisor
-            n //= divisor
-        divisor += 1
+    for i in range(2, int(math.sqrt(n)) + 1):
+        if n % i == 0:
+            min_ops = min(min_ops, i + minOperations(n // i))
 
-    return operations
+    if min_ops == float('inf'):
+        min_ops = n
+
+    return min_ops
 
 
 if __name__ == "__main__":
