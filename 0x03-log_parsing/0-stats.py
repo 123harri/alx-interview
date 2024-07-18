b4dafa9b67342f57665c6baa0ce7a474b01c1310
123harri <harrietwanjiku90@gmail.com>
2024-07-18 10:51:50 +0000

    a

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-A script that reads stdin line by line and computes metrics.
+Script that reads stdin line by line and computes metrics.
 """
 
 import sys
@@ -34,14 +34,7 @@ def parse_line(line):
 
 def main():
     total_size = 0
-    status_counts = {"200": 0,
-                     "301": 0,
-                     "400": 0,
-                     "401": 0,
-                     "403": 0,
-                     "404": 0,
-                     "405": 0,
-                     "500": 0}
+    status_counts = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
     line_count = 0
 
     try:
@@ -61,7 +54,3 @@ def main():
         raise
 
     print_stats(total_size, status_counts)
-
-
-if __name__ == "__main__":
-    main()
